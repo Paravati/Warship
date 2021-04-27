0fc59a8afc1ce80fe178aaebdfc4c1b8c63febae
Patrycja <pati.iwanek@gmail.com>
2021-04-27 21:42:16 +0200

    adding first ship into gameboard

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        ShipmentInitialization shipmentInitialization = new ShipmentInitialization();
         String[][] gameboard = initBoard(11, 11);
+        ShipmentInitialization shipmentInitialization = new ShipmentInitialization();
         drawBoard(gameboard);
-//        ship.putShip();
-    }
-    
-    public static String[][] initBoard(int x, int y){
-        String[][] gameMap = new String[x][y];
-        for(int i=0; i<x; i++){
-            for(int j=0; j<y; j++) {
-                gameMap[i][j] = "| ~ ";
-            }
-        }
-        
-        return gameMap;
+        ShipmentInitialization.begin(gameboard);
+        drawBoard(gameboard);
     }
 
+    public static String[][] initBoard(int x, int y){
+        String[][] gameMap = new String[x][y];
+        int firstAsciiCharacter = 65;  //65 is first ascii char
+        for(int i=0; i<x; i++){
+            for(int j=0; j<y; j++) {
+                gameMap[i][j] = Character.toString((char) firstAsciiCharacter+j) + (i+1);
+            }
+        }
+        return gameMap;
+    }
+//    public static String[][] initBoard(int x, int y){
+//        String[][] gameMap = new String[x][y];
+//        for(int i=0; i<x; i++){
+//            for(int j=0; j<y; j++) {
+//                gameMap[i][j] = "| ~ ";
+//            }
+//        }
+//
+//        return gameMap;
+//    }
+
     public static void drawBoard(String[][] gameMap){
-        String[] notationHorizontal = new String[gameMap.length];
         int firstAsciiChar = 65;
         for(int i=0; i<gameMap.length; i++){
             if(i==0){
@@ -42,7 +52,12 @@ public class Main {
                         System.out.print("  " + (i+1) + " ");
                     }
                 }
-                System.out.print(gameMap[i][j]);
+//                System.out.print(gameMap[i][j]);
+                if (gameMap[i][j].equals("N")){
+                    System.out.print("|OOO");
+                }else{
+                    System.out.print("| ~ ");
+                }
             }
             System.out.println("|");
         }
--- src/com/company/ShipmentInitialization.java
+++ src/com/company/ShipmentInitialization.java
@@ -1,9 +1,29 @@
 package com.company;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ShipmentInitialization {
-    public static void putShip(int x, int y, int direction, typesOfShip type){
-        if (type == typesOfShip.CRUISER){
+    public static void begin(String[][] board){
+        System.out.print("Hello!\nWe need to install your ships. Please insert place for first Cruiser: ");
+        Scanner input = new Scanner(System.in);
+        String cruiser_1 = input.next();
+        System.out.println("This is location for 1st cruiser: " + cruiser_1);
+        putShip(board, cruiser_1, 0, typesOfShip.CRUISER);
+    }
 
+    public static void putShip(String[][] board, String firstField, int direction, typesOfShip type){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[i].length; j++){
+                if(board[j][i].equals(firstField)){
+                    board[j][i] = "N"; // this is ship symbol - only for this moment
+                    System.out.println("Locked localisations: " + (j-1) + (i));
+                }
+            }
+        }
+    }
+
+    public static void defineLengthOfTheShip(typesOfShip type){
+        if (type == typesOfShip.CRUISER){
         }
     }
 }
